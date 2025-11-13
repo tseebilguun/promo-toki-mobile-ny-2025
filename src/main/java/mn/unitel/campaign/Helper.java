9fbd273d6a89d09e525fd45a98cc41bf698ffd85
TsE <tsee.bilguun@gmail.com>
2025-11-13 17:12:56 +0800

    Test

--- src/main/java/mn/unitel/campaign/Helper.java
+++ src/main/java/mn/unitel/campaign/Helper.java
@@ -1,9 +1,16 @@
 package mn.unitel.campaign;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class Helper {
+    Logger logger = Logger.getLogger(Helper.class.getName());
+
+    @ConfigProperty(name = "blacklist.numbers.path", defaultValue = "config/blacklist-numbers.json")
+    String blacklistNumbersPath;
+
     public boolean isTokiNumber(String msisdn) {
         return msisdn.matches("^(50[0-4]|55[0-4]).*");
     }
