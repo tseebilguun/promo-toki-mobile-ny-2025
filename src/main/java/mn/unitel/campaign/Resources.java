66bba09bdce594159d835c68bf9488593910eb99
tseebilguun <tse4game@gmail.com>
2025-11-14 13:55:46 +0800

    JWT updated

--- src/main/java/mn/unitel/campaign/Resources.java
+++ src/main/java/mn/unitel/campaign/Resources.java
@@ -1,23 +1,30 @@
 package mn.unitel.campaign;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import mn.unitel.campaign.services.SpinnerService;
+import mn.unitel.campaign.models.LoginReq;
+import mn.unitel.campaign.services.AuthService;
 
 @Path("/")
 @Consumes("application/json")
 @Produces("application/json")
 public class Resources {
     @Inject
-    SpinnerService spinnerService;
+    AuthService authService;
 
     @POST
-    @Path("/helloWorld")
-    public Response helloWorld() {
-        return Response.ok("Hello, World").build();
+    @Path("/auth/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(LoginReq loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    // Front end will call this API with the jwt token that was received from /auth/login
+    @GET
+    @Path("/spinnger/getInfo")
+    public Response getGeneralInfo() {
+
     }
 }
